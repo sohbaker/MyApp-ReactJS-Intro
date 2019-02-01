36bd8bb9d67e8a8d2c3782cfde1e1e260037eace
Tola's Terminal <sohbaker@gmail.com>
2019-02-01 13:05:08 +0000

    second test passed for board.js

--- src/__tests__/board.test.js
+++ src/__tests__/board.test.js
@@ -5,3 +5,10 @@ import { Board } from '../board';
  it('renders without crashing', () => {
   shallow(<Board squares={[0, 1, 2, 3, 4, 5, 6, 7, 8]} />);
 });
+
+it('renders squares values of 0 to 8', () => {
+ const wrapper = shallow(<Board squares={[0, 1, 2, 3, 4, 5, 6, 7, 8]} />);
+ wrapper.debug();
+ expect(wrapper.find('Square').at(0).prop('value')).toBe(0)
+ expect(wrapper.find('Square').at(8).prop('value')).toBe(8)
+});
